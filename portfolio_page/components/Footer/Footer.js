fb8fa9fc7d631c531f7dd7dac2d1f546f25c6df2
gulhayoarabova <gulhayoarabova@gmail.com>
2022-09-27 16:43:00 +0500

    completed

---
+++ portfolio_page/components/Footer/Footer.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import classes from "./Footer.module.scss"
+import logo from "../../assets/images/logo.png"
+import face from "../../assets/icons/face.png"
+import linkedin from "../../assets/icons/Linkedin.png"
+import telegram from "../../assets/icons/telegram-24.png"
+import twitter from "../../assets/icons/twitter.png"
+import Link from "next/link"
+import Image from "next/image";
+
+const Footer = () => {
+    return (
+        <div className={classes.footer_wrapper}>
+            <div className={classes.inner_wrap}>
+            <Image src={logo} />
+            <p className={classes.lrg_txt}>Lets Talk with Me!</p>
+            <p className={classes.sml_txt}>I am always open to discuss your project, improve your online presence or help with your UX/UI design</p>
+            <div className={classes.icon_wrap}>
+                <div>
+                <Link href={"https://www.facebook.com/profile.php?id=100077345967018"}>
+                    <Image src={ face} />
+                </Link>
+                </div>
+                <div>
+                 <Link href={"https://twitter.com/Gulhayo04"}>
+                    <Image src={twitter } />
+                </Link>
+                </div>
+                <div>
+                 <Link href={"https://t.me/gulhay0"}>
+                    <Image src={telegram } />
+                </Link>
+                </div>
+                <div>
+                 <Link href={"https://www.linkedin.com/in/gulhayo-arabova-b865a3210/"}>
+                    <Image src={linkedin } />
+                </Link>
+                </div>
+            </div>
+            </div>
+            <div className={classes.inner2_wrap}>
+                <ul>
+                    <li>Start</li>
+                    <li>Services</li>
+                    <li>Portfolio</li>
+                    <li>News</li>
+                    <li>Contact</li>
+                </ul>
+                <Link href={"gulhayoarabova@gmail.com"}>
+                <p className={classes.email}>gulhayoarabova@gmail.com</p>
+                </Link>
+            </div>
+
+        </div>
+    )
+}
+
+export default Footer
--- portfolio_page/pages/index.js
+++ portfolio_page/pages/index.js
@@ -1,13 +1,147 @@
-import React from "react"
-import Header from "../components/Header/Header"
-import classes from "../assets/styles/Home.module.scss"
-// import "../assets/styles/globals.css"
+import React from "react";
+import Header from "../components/Header/Header";
+import classes from "../assets/styles/Home.module.scss";
+import Link from "next/link"
+import Image from "next/image";
+import arrow from "../assets/icons/arrow.png";
+import illustration from "../assets/images/illustration.png";
+import illustrations from "../assets/images/illustrations.png";
+import pizza from "../assets/images/pizza.jpg";
+import desktop from "../assets/images/desktop.png";
+import Footer from "../components/Footer/Footer";
+
+
+
+
 export default function Home() {
+
+
   return (
     <div className={classes.home_wrapper}>
-    <div className={classes.container}>
-   <Header/>
+      <div className={classes.container}>
+        <Header />
+        <div className={classes.banner_wrapper}>
+          <div className={classes.left_txt}>
+            <p className={classes.red_txt}>FRONTEND DEVELOPER</p>
+            <h2>Fast working and high quality projects</h2>
+            <p className={classes.description}>
+              Hi, I’m Gulhayo. I’m a Frontend developer. If you are looking for
+              deevloper to build your websites and grow your business Let’s
+              shake hands with me.
+            </p>
+            <div className={classes.hire_section}>
+              <button>
+                <Image src={arrow} width={20} height={20} />
+              </button>
+              <p>Hire me now</p>
+            </div>
+            <p className={classes.proud}>We are proud of working with you</p>
+          </div>
+          <div className={classes.img_section}>
+            <Image src={illustration} />
+          </div>
+          <div className={classes.right_txt}>
+            <p className={classes.txt1}>Portfolio Landing UI</p>
+            <p className={classes.txt2} >Explore resume</p>
+          </div>
+        </div>
+        <div className={classes.clients_wrapper}>
+          <div className={classes.img_section}>
+            <Image src={illustrations} />
+          </div>
+          <div className={classes.right_promise_section}>
+            <p className={classes.promise}>
+              From Big to Small Clients,
+              Two Things I Always Promise
+            </p>
+            <div className={classes.divs}>
+              <div className={classes.lft_section}>
+                <p className={classes.num1}>1</p>
+                <div className={classes.inner_txt1}>
+                  <p className={classes.txt1}>Fast and effectively working website</p>
+                  <p className={classes.txt2}>Donec metuse, vulputate at sapiens sit amet, auctor iaculis lorem. In the hendrerit nisi.</p>
+                </div>
+              </div>
+
+              <div className={classes.rght_section}>
+                <p className={classes.num1}>2</p>
+                <div className={classes.inner_txt2}>
+                  <p className={classes.txt1}>Friendly Relationship</p>
+                  <p className={classes.txt2}>Donec metuse, vulputate at sapiens sit amet, auctor iaculis lorem. In the hendrerit nisi.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={classes.projects}>
+          <p className={classes.title}>
+            Projects
+          </p>
+          <div className={classes.project_card_wrapper}>
+            <div className={classes.project_card}>
+              <Image src={pizza} width={280} height={190} />
+              <p className={classes.border_top}>Pizza Delivery:  Chopar Pizza</p>
+              <Link href="youtube.com">
+                <p>Visit website ...</p>
+              </Link>
+            </div>
+
+
+            <div className={classes.project_card}>
+              <Image src={pizza} width={280} height={190} />
+              <p className={classes.border_top}>Pizza Delivery:  Chopar Pizza</p>
+              <Link href="youtube.com">
+                <p>Visit website ...</p>
+              </Link>
+            </div>
+
+            <div className={classes.project_card}>
+              <Image src={pizza} width={280} height={190} />
+              <p className={classes.border_top}>Pizza Delivery:  Chopar Pizza</p>
+              <Link href="youtube.com">
+                <p>Visit website ...</p>
+              </Link>
+            </div>
+
+            <div className={classes.project_card}>
+              <Image src={pizza} width={280} height={190} />
+              <p className={classes.border_top}>Pizza Delivery:  Chopar Pizza</p>
+              <Link href="youtube.com">
+                <p>Visit website ...</p>
+              </Link>
+            </div>
+
+            <div className={classes.project_card}>
+              <Image src={pizza} width={280} height={190} />
+              <p className={classes.border_top}>Pizza Delivery:  Chopar Pizza</p>
+              <Link href="youtube.com">
+                <p>Visit website ...</p>
+              </Link>
+            </div>
+
+            <div className={classes.project_card}>
+              <Image src={pizza} width={280} height={190} />
+              <p className={classes.border_top}>Pizza Delivery:  Chopar Pizza</p>
+              <Link href="youtube.com">
+                <p>Visit website ...</p>
+              </Link>
+            </div>
+
+          </div>
+        </div>
+        <div className={classes.get_touch}>
+          <Image src={ desktop} width={320} height={340}/>
+          <div className={classes.txt_wrap}>
+            <p className={classes.bold}>Have Any Project in Mind?</p>
+            <p className={classes.bold}>Don’t Be Hesitate to Contact Us</p>
+            <p className={classes.light}>Ready to make something creative?
+              Let's get on a call.</p>
+            <button className={classes.touch_btn}>Get in touch</button>
+          </div>
+        </div>
+        <Footer/>
+        <p className={classes.copyright}>Copyright © 2022 by Gulhayo Arabova. All Rights Reserved.</p>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
